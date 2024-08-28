16d7303017129d28cca4dd9888533e43c9d68fe7
third-wind <amador.raga@gmail.com>
2024-08-28 04:21:57 -0700

    Added HelperService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HelpersService} from "./helpers.service";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
+  constructor(public svcHelpers: HelpersService) {
+
+  }
   title = 'ANGULAR';
+  ngOnInit() {
+    for(let x=0; x<100; x++) {
+      console.log(this.svcHelpers.randomRange(10,15,4));
+    }
+  }
 }
